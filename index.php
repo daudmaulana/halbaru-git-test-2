07c74a09977fb0e357ac924da7b3b511c993bd4c
Daud Maulana <daudmaulana321@gmail.com>
2023-11-23 16:32:02 +0700

    Menambah paragraf pada index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Oiii</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo eum molestias debitis, similique corporis commodi incidunt eligendi quibusdam deserunt dicta vitae nam modi, libero suscipit temporibus quisquam voluptatibus error.</p>
 </body>
 </html>
