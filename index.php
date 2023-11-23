69afe37900fb793789cd687c4e11ba608fb2b666
Daud Maulana <daudmaulana321@gmail.com>
2023-11-23 16:36:57 +0700

    Menambah list pada index.php

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Oiii</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo eum molestias debitis, similique corporis commodi incidunt eligendi quibusdam deserunt dicta vitae nam modi, libero suscipit temporibus quisquam voluptatibus error.</p>
+    <ul>
+        <li>pp</li>
+    </ul>
 </body>
 </html>
